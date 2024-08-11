d6721862d84aaf7d4dad9e448c4d04e5dc005ca2
RazielApple <fabio.barbato2@studenti.unina.it>
2024-08-11 22:23:13 +0200

    parsing json from server

--- Lso_Client/Lso_Client/Connection.swift
+++ Lso_Client/Lso_Client/Connection.swift
@@ -3,7 +3,7 @@ import Network
 
 class NetworkManager: ObservableObject {
     static let shared = NetworkManager()
-    
+    @Published var bookCatalog: [Book] = []
     private let host_ip = "192.168.1.116"
     private let host = NWEndpoint.Host("192.168.1.116")
     private let port = NWEndpoint.Port(rawValue: 8080)!
@@ -49,13 +49,6 @@ class NetworkManager: ObservableObject {
         }
     }
     
-    func login(username: String, password: String){
-        let message = "LOGIN \(username) \(password)"
-        if let messageData = message.data(using: .utf8) {
-            send(data: messageData)
-        }
-        receive()
-    }
 
     func register(name: String, surname: String, username: String, password: String){
         let message = "LOGIN \(name) \(surname) \(username) \(password)"
@@ -64,4 +57,30 @@ class NetworkManager: ObservableObject {
         }
         receive()
     }
+    private func parseReceivedData(_ data: Data) {
+        let responseString = String(data: data, encoding: .utf8) ?? ""
+        print("Response received: \(responseString)")
+
+        // Parse JSON data
+        if let data = responseString.data(using: .utf8) {
+            do {
+                let books = try JSONDecoder().decode([Book].self, from: data)
+                DispatchQueue.main.async {
+                    self.bookCatalog = books
+                }
+            } catch {
+                print("Error decoding JSON: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+struct Book: Identifiable, Codable {
+    var id = UUID()
+    let title: String
+    let ISBN: String
+    let author: String
+    let genre: String
+    let copies: Int
+    let given_copies: Int
 }
