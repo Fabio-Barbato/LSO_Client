bf926c5351a477468434477f7947bb469416fecc
RazielApple <fabio.barbato2@studenti.unina.it>
2024-08-11 11:34:28 +0200

    first commit

---
+++ LSO_Client/LSO_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  LSO_ClientApp.swift
+//  LSO_Client
+//
+//  Created by Fabio Barbato on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LSO_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
